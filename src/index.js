5a4e38b5ef63aba82d491a973623bf55a44d7566
addierohrbach <adelinerohrbach2020@u.northwestern.edu>
2019-04-11 13:17:34 -0500

    Adds product.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+fetch('./data/products.json')
+.then(resp => resp.json())
+.then(json => {
+   ReactDOM.render(<App products={json} />, document.getElementById('root'));
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
